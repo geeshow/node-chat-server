51a873b56f360244439fe7a47520d62d0331d3a0
kyutae <ken2484@gmail.com>
2023-11-06 13:46:25 +0900

    fix: change port to 5000

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import {WebSocketServer} from "ws";
 import config from "../config.json";
 import WebSocketHandler from "./socket/WebSocketHandler";
+import * as http from "http";
 
-const wss = new WebSocketServer({ port: config.port })
+console.log(`Server started on port ${config.port}`)
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+});
+
+const wss = new WebSocketServer({ server })
 wss.on('connection', (ws, request) => {
     try {
         const token = request.url?.split('=')[1] ?? '';
@@ -13,4 +21,4 @@ wss.on('connection', (ws, request) => {
     }
 });
 
-console.log(`Server started on port ${config.port}`)
+server.listen(config.port);
